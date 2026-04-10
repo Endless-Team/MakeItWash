a14c3b54cfccbff673ace8eed3f043dcf44bff30
TalpaDev <luppialessandro@istitutocorni.it>
2026-04-10 12:16:49 +0200

    Refactoring

--- core/src/com/makeitwash/items/DirtyClothing.java
+++
@@ -1,9 +0,0 @@
-package com.makeitwash.items;
-
-public class DirtyClothing extends Item {
-    public static final float BASE_VALUE = 10f;
-
-    public DirtyClothing() {
-        super("Vestito Sporco", BASE_VALUE);
-    }
-}
