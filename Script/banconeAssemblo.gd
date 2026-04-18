2cfc3f6bd0157826bcd093247a7b4b4d7d044f9a
FireProj <morganmalavasi@gmail.com>
2026-04-18 15:50:19 +0200

    diocane di cliente

--- Script/banconeAssemblo.gd
+++ Script/banconeAssemblo.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var tipo: String = "assemblo"
+
 func _ready() -> void:
 	add_to_group("bancone_assemblo")
 	pass
@@ -11,3 +13,7 @@ func _on_body_entered(body) -> void:
 func _on_body_exited(body) -> void:
 	if body.has_method("set_vicino_bancone"):
 		body.set_vicino_bancone(false, "")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
