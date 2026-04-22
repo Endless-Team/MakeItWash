5f2f57515238af3b45447616cbcef522fdad3d80
FireProj <morganmalavasi@gmail.com>
2026-04-22 22:49:52 +0200

    update quasi finale

--- scripts/moneyLabel.gd
+++ scripts/moneyLabel.gd
@@ -1,9 +1,10 @@
-# feat: mostra anche preparazioni e dettaglio piatti nell'inventario
+# fix: money label mostra solo i soldi, senza stile nel codice
 extends Label
 
 
 func _ready() -> void:
 	_aggiorna_testo()
+
 	if not Inventario.inventario_cambiato.is_connected(_on_inventario_cambiato):
 		Inventario.inventario_cambiato.connect(_on_inventario_cambiato)
 
@@ -13,31 +14,4 @@ func _on_inventario_cambiato() -> void:
 
 
 func _aggiorna_testo() -> void:
-	var salmone: int = int(Inventario.ingredienti.get("Salmone", 0))
-	var gambero: int = int(Inventario.ingredienti.get("Gambero", 0))
-	var osomaki_intero: int = int(Inventario.preparazioni.get("Osomaki intero", 0))
-
-	var nigiri_salmone := 0
-	var nigiri_gambero := 0
-	var osomaki := 0
-
-	for piatto in Inventario.piatti_pronti:
-		match piatto:
-			"Nigiri salmone":
-				nigiri_salmone += 1
-			"Nigiri gambero":
-				nigiri_gambero += 1
-			"Osomaki":
-				osomaki += 1
-
-	var piatti_totali: int = Inventario.piatti_pronti.size()
-
-	text = "Inventario\nSalmone: %d\nGambero: %d\nOsomaki intero: %d\n\nPiatti pronti: %d\nNigiri salmone: %d\nNigiri gambero: %d\nOsomaki: %d" % [
-		salmone,
-		gambero,
-		osomaki_intero,
-		piatti_totali,
-		nigiri_salmone,
-		nigiri_gambero,
-		osomaki
-	]
+	text = "Soldi: %d" % Inventario.soldi
